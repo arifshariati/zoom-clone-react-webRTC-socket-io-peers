726f4bf1d52844d5380433487ea4377d537bd94c
Arif Shariati <arif.shariati@kns.com.tr>
2021-06-04 06:49:09 +0300

    socket connection and handlers

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,23 @@ app.get("/", (req, res) => {
     res.send("Server is Up...");
 });
 
+io.on('connection', (socket) =>{
+
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit('callended');
+    });
+
+    socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+    });
+
+    socket.on('answercall', (data) => {
+        io.to(data.to).emit('callaccepted', data.signal);
+    });
+    
+});
+
+
 server.listen(PORT, ()=> console.log(`Server is listening on port ${PORT}`));
